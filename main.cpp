277dd3238ce20b5d2d015804430a9253c664d66c
gfl <lammersdorf@gmx.de>
2023-11-27 14:13:35 +0100

    ji

--- main.cpp
+++ main.cpp
@@ -3,10 +3,7 @@
 #include <iostream>
 #include <QString>
 
-QString changeIntToHex(int in);
-QString changeStrToHex(QString in);
-char intToHex(int i);
-char charToHex(char c);
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
